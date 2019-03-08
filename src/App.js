5950a000381e2313fee9779302ef8bb7f5f1d200
SheaParrott <alexandergotthis@gmail.com>
2019-03-08 18:38:30 -0500

    to do list created

--- src/App.js
+++ src/App.js
@@ -9,3 +9,23 @@ class App extends Component {
 }
 
 export default App
+
+// [] turn in assignment to jobs@trifinlabs.com
+// [] email subject - Technical Interview Assignment - Shea Parrott
+// [] post link to GitHub repo in email body
+
+// [] create boxes that hold events for the days - one week worth
+// [] Should only have one event running at the same time?
+// [] validate all entries
+// [] make all entries CRUD - create, read, update, delete
+// [] event object {event: “ “, date: MMDDYY, time: 0:00, isWeekend: bool }
+// [] display blocks for available events. Will probably do this in 30 min intervals
+// [] make visual difference for weekend boxes
+
+// push goals - optional
+// [] toggle between week and day views
+// [] theme appointments by color
+// [] request confirmations before deleting event
+
+// notes:
+// no restriction on data model. Make sure to optimize complexity when accessing record entries
