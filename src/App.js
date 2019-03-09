5e8f3d7c7599fcf56a1c500e9d63da53e821b9d3
SheaParrott <alexandergotthis@gmail.com>
2019-03-09 13:09:24 -0500

    core functionality of search date bar working with state

--- src/App.js
+++ src/App.js
@@ -9,6 +9,26 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.onUpdateStateCurrentDay = this.onUpdateStateCurrentDay.bind(this)
+    this.state = {
+      months: [
+        { number: 0, name: 'January', days: 31 },
+        { number: 1, name: 'February', days: 28 },
+        { number: 2, name: 'March', days: 31 },
+        { number: 3, name: 'April', days: 30 },
+        { number: 4, name: 'May', days: 31 },
+        { number: 5, name: 'June', days: 30 },
+        { number: 6, name: 'July', days: 31 },
+        { number: 7, name: 'August', days: 31 },
+        { number: 8, name: 'September', days: 30 },
+        { number: 9, name: 'October', days: 31 },
+        { number: 10, name: 'November', days: 30 },
+        { number: 11, name: 'December', days: 31 }
+      ],
+      monthChosen: '',
+      dayChosen: '',
+      yearChosen: '',
+      weekOrDayChosen: ''
+    }
   }
   // componentDidMount = () => {
   //   this.onUpdateStateCurrentDay()
@@ -16,43 +36,97 @@ class App extends Component {
   onUpdateStateCurrentDay() {
     this.props.onUpdateStateCurrentDay(new Date())
   }
-  monthDays = event => {
-    console.log(event.target.value)
-    console.log(new Date(event.target.value))
-  }
   test = () => {
     console.log(new Date(2018, 0, 1))
   }
-  getDay = () => {}
+  updateMonthChosen = event => {
+    let theMonth = this.state.months.filter(
+      month => month.name === event.target.value
+    )
+    this.setState({
+      monthChosen: theMonth
+    })
+  }
+  updateDayChosen = event => {
+    this.setState({
+      dayChosen: event.target.value
+    })
+  }
+  updateYearChosen = event => {
+    this.setState({
+      yearChosen: event.target.value
+    })
+  }
+  updateWeekOrDayChosen = event => {
+    this.setState({
+      weekOrDayChosen: event.target.value
+    })
+  }
+  updateFullDate = () => {
+    console.log(this.state.monthChosen)
+    console.log(this.state.dayChosen)
+    console.log(this.state.yearChosen)
+    console.log(this.state.weekOrDayChosen)
+    // monthChosen: '',
+    // dayChosen: '',
+    // yearChosen: '',
+    // weekOrDayChosen: ''
+  }
   render() {
-    console.log(this.props.months)
+    // console.log(this.props.months)
     return (
       <div className="App">
         <button onClick={this.test}>test</button>
         <div className="inputContainer">
-          <input onChange={this.monthDays} placeholder="month" list="months" />
+          <input
+            onChange={this.updateMonthChosen}
+            placeholder="month"
+            list="months"
+          />
           <datalist id="months">
-            {this.props.months.map((month, index) => {
+            {this.state.months.map((month, index) => {
               return <option key={index} value={month.name} />
             })}
           </datalist>
-          <input list="days" placeholder="day" />
+          <input
+            onChange={this.updateDayChosen}
+            list="days"
+            placeholder="day"
+          />
           <datalist id="days">
-            <option value="1" />
+            {this.state.monthChosen
+              ? this.state.monthChosen.map(days => {
+                  let array = []
+                  for (let i = 1; i <= days.days; i++) {
+                    array.push(i)
+                  }
+                  return array.map((number, index) => {
+                    return <option key={index} value={number} />
+                  })
+                })
+              : null}
           </datalist>
-          <input list="year" placeholder="year" />
+          <input
+            onChange={this.updateYearChosen}
+            list="year"
+            placeholder="year"
+          />
           <datalist id="year">
             <option value="2018" />
             <option value="2019" />
             <option value="2020" />
             <option value="2021" />
           </datalist>
-          <input list="weekOrDay" placeholder="W/D" />
+          <input
+            onChange={this.updateWeekOrDayChosen}
+            list="weekOrDay"
+            placeholder="W/D"
+          />
           <datalist id="weekOrDay">
             <option value="Day" />
             <option value="Week" />
           </datalist>
-          <button onClick={this.getDay}>Search</button>
+          <button onClick={this.updateFullDate}>Search</button>
         </div>
         <button onClick={this.onUpdateStateCurrentDay} />
         {this.props.day}
@@ -62,8 +136,7 @@ class App extends Component {
   }
 }
 const mapStateToProps = state => ({
-  day: state.day.toLocaleString(),
-  months: state.months
+  day: state.day.toLocaleString()
 })
 
 const mapActionsToProps = { onUpdateStateCurrentDay: updateCurrentDay }
--- src/index.js
+++ src/index.js
@@ -13,26 +13,14 @@ function getSunday(d) {
     diff = d.getDate() - day
   return new Date(d.setDate(diff))
 }
-let months = [
-  { number: 0, name: 'January', days: 31 },
-  { number: 1, name: 'February', days: 28 },
-  { number: 2, name: 'March', days: 31 },
-  { number: 3, name: 'April', days: 30 },
-  { number: 4, name: 'May', days: 31 },
-  { number: 5, name: 'June', days: 30 },
-  { number: 6, name: 'July', days: 31 },
-  { number: 7, name: 'August', days: 31 },
-  { number: 8, name: 'September', days: 30 },
-  { number: 9, name: 'October', days: 31 },
-  { number: 10, name: 'November', days: 30 },
-  { number: 11, name: 'December', days: 31 }
-]
 
+const allReducers = combineReducers({
+  day: currentDayReducer
+})
 const store = createStore(
-  currentDayReducer,
+  allReducers,
   {
-    day: getSunday(new Date()),
-    months: months
+    day: getSunday(new Date())
   },
   window.devToolsExtension && window.devToolsExtension()
 )
