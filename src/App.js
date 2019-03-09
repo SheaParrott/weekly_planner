621e255fce64419d6ebc5e560533b5395f52de06
SheaParrott <alexandergotthis@gmail.com>
2019-03-09 11:01:43 -0500

    added array for months of the year with available days. basic CSS and layout for searching day created

--- src/App.js
+++ src/App.js
@@ -16,11 +16,43 @@ class App extends Component {
   onUpdateStateCurrentDay() {
     this.props.onUpdateStateCurrentDay(new Date())
   }
+  monthDays = event => {
+    console.log(event.target.value)
+    console.log(new Date(event.target.value))
+  }
+  test = () => {
+    console.log(new Date(2018, 0, 1))
+  }
+  getDay = () => {}
   render() {
+    console.log(this.props.months)
     return (
       <div className="App">
-        <div>
-          <input />
+        <button onClick={this.test}>test</button>
+        <div className="inputContainer">
+          <input onChange={this.monthDays} placeholder="month" list="months" />
+          <datalist id="months">
+            {this.props.months.map((month, index) => {
+              return <option key={index} value={month.name} />
+            })}
+          </datalist>
+          <input list="days" placeholder="day" />
+          <datalist id="days">
+            <option value="1" />
+          </datalist>
+          <input list="year" placeholder="year" />
+          <datalist id="year">
+            <option value="2018" />
+            <option value="2019" />
+            <option value="2020" />
+            <option value="2021" />
+          </datalist>
+          <input list="weekOrDay" placeholder="W/D" />
+          <datalist id="weekOrDay">
+            <option value="Day" />
+            <option value="Week" />
+          </datalist>
+          <button onClick={this.getDay}>Search</button>
         </div>
         <button onClick={this.onUpdateStateCurrentDay} />
         {this.props.day}
@@ -30,7 +62,8 @@ class App extends Component {
   }
 }
 const mapStateToProps = state => ({
-  day: state.day.toLocaleString()
+  day: state.day.toLocaleString(),
+  months: state.months
 })
 
 const mapActionsToProps = { onUpdateStateCurrentDay: updateCurrentDay }
@@ -39,6 +72,11 @@ export default connect(
   mapStateToProps,
   mapActionsToProps
 )(App)
+
+// to get choose date bar working
+// [] chosen month
+// [] chosen day in state
+// [] chosen year
 
 // [] turn in assignment to jobs@trifinlabs.com
 // [] email subject - Technical Interview Assignment - Shea Parrott
--- src/index.js
+++ src/index.js
@@ -13,11 +13,26 @@ function getSunday(d) {
     diff = d.getDate() - day
   return new Date(d.setDate(diff))
 }
+let months = [
+  { number: 0, name: 'January', days: 31 },
+  { number: 1, name: 'February', days: 28 },
+  { number: 2, name: 'March', days: 31 },
+  { number: 3, name: 'April', days: 30 },
+  { number: 4, name: 'May', days: 31 },
+  { number: 5, name: 'June', days: 30 },
+  { number: 6, name: 'July', days: 31 },
+  { number: 7, name: 'August', days: 31 },
+  { number: 8, name: 'September', days: 30 },
+  { number: 9, name: 'October', days: 31 },
+  { number: 10, name: 'November', days: 30 },
+  { number: 11, name: 'December', days: 31 }
+]
 
 const store = createStore(
   currentDayReducer,
   {
-    day: getSunday(new Date())
+    day: getSunday(new Date()),
+    months: months
   },
   window.devToolsExtension && window.devToolsExtension()
 )
