16e9bf443d449cc53ce2f3e7769334c5c3eda395
SheaParrott <alexandergotthis@gmail.com>
2019-03-09 22:53:35 -0500

    user can now update the week dynamically by choosing a day.

--- src/Actions/updateFirstDayOfWeek.js
+++ src/Actions/updateFirstDayOfWeek.js
@@ -4,11 +4,33 @@ export const FIRST_DAY_OF_WEEK = 'firstDayOfWeek'
 // this takes the chosen day and finds the first day of the week
 // so we can get the week generated and add to state.
 
+// this is working but after the first change in the selected week.
+// it is concating the arrays. I want it to drop the array. then
+// reset the state
+let daysShown = []
+
+function week(firstDayOfWeek) {
+  console.log(firstDayOfWeek)
+  for (let i = 0; i < 7; i++) {
+    let date = firstDayOfWeek.toISOString().split('')
+    let day = date[8] + date[9]
+    day = parseInt(day) + i
+    let newDate = new Date(
+      date.slice(0, 4).join(''),
+      date.slice(5, 7).join('') - 1,
+      day
+    )
+    daysShown.push(newDate)
+  }
+}
+
 export function updateFirstDayOfWeek(currentDay) {
+  week(currentDay)
   return {
     type: FIRST_DAY_OF_WEEK,
     payload: {
-      firstDayOfWeek: currentDay
+      firstDayOfWeek: currentDay,
+      daysShown: daysShown
     }
   }
 }
